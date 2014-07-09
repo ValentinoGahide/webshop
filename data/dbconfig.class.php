5b552a80b5a5805d82023039fdd03d0240aff9a3
ValentinoGahide <valentinogahide@gmail.com>
2014-07-09 13:22:13 +0200

    fixen pathing

--- data/dbconfig.class.php
+++ data/dbconfig.class.php
@@ -3,7 +3,7 @@
 class DBConfig {
 
     public static $DB_CONNSTRING = "mysql:host=localhost;dbname=webshop";
-    public static $DB_USERNAME = "cursusgebruiker";
-    public static $DB_PASSWORD = "cursuspwd";
+    public static $DB_USERNAME = "root";
+    public static $DB_PASSWORD = "";
 
 }
