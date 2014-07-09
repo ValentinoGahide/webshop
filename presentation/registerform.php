cc993e9daf72525afaa11143e61bcf39456d2638
ValentinoGahide <valentinogahide@gmail.com>
2014-07-09 14:37:24 +0200

    adding registration

--- presentation/registerform.php
+++ presentation/registerform.php
@@ -3,6 +3,13 @@
     <head><title>Webshop</title></head>
     <body>
         <h1>Registreren</h1>
-
-</body>
+        <form action="index.php?action=register" method="post">
+            <label>Kies een gebruikersnaam: </label>
+            <input type="text" name="txtGebruikersnaam" placeholder="gebruikersnaam"><br>
+            <label>Kies een wachtwoord: </label>
+            <input type="password" name="txtWachtwoord" placeholder="wachtwoord"><br>
+            <input type="submit" value="Bevestig">
+            <input type="reset" value="Herbegin">
+        </form>
+    </body>
 </html>
